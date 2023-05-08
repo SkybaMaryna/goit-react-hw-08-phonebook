b909b024e07be6977c3a683f4389a378c64af023
Your Name~ <122438581+SkybaMaryna@users.noreply.github.com>
2023-05-08 14:00:45 +0300

    update

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,7 @@
 export const HomePage = () => {
   return (
     <p className="flex justify-center items-center h-screen font-bold text-4xl bg-gradient-to-r from-cyan-200 to-blue-200">
-      Welcome on board
+      Welcome to your Phonebook
     </p>
   );
 };
